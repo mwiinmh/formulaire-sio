8c1464dd3ae1c02fa03df7a993d2fcdc27df7b96
mwiinmhe <mwindjoummhoumadi78@hotmail.com>
2025-02-11 15:15:42 +0100

    md,html,js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", (e)=>{
     e.preventDefault()
 
+    const inputName = document.querySelectorAll('input[type="text"]')
+
     const btn_submit = document.getElementsByTagName('input')[3]
     btn_submit.addEventListener("click", (e)=>{
-      alert
-    })
+        e.preventDefault()
+      /*  Swal.fire({
+            title: "Good job!",
+            text: "You clicked the button!",
+            icon: "success"
+          }); */
+    }) 
 })
