df506a7b3bd1abe01ba0ba11eb15afe6bf1fca1e
mwiinmhe <mwindjoummhoumadi78@hotmail.com>
2025-02-11 15:00:27 +0100

    html, md, js

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # 🚀 Formulaire client : inscription
-
+[voir la publication](http://127.0.0.1:5501/)
 + **Langage**
 * HTML
 * CSS
---
+++ src/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", (e)=>{
+    e.preventDefault()
+
+    const btn_submit = document.getElementsByTagName('input')[3]
+    btn_submit.addEventListener("click", (e)=>{
+      alert
+    })
+})
